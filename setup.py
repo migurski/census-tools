15964fb8d6ea0cf679b1e1aa4e30a2d39a1edfe6
Michal Migurski <mike@stamen.com>
2010-08-10 21:41:20 -0700

    distutils I love you

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+version = '0.1.0'
+
+setup(name='census-tools',
+      version=version,
+      description='...',
+      author='Michal Migurski',
+      author_email='mike@stamen.com',
+      url='http://github.com/migurski/census-tools',
+      packages=[],
+      scripts=['census2text.py'],
+      data_files=[],
+      download_url='',
+      license='BSD')
